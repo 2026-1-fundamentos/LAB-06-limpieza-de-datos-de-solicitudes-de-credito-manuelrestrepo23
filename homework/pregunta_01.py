c5e5944cc01d11b5390dc67453fdccc2bb23afc4
manuelrestrepo23 <mrestreposi@unal.edu.co>
2026-06-24 18:21:49 -0500

    lab6int2

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -5,36 +5,55 @@ import os
 import pandas as pd
 
 def load_data(input_file):
+
     df = pd.read_csv(input_file, sep=";")
     return df
 
 
 def clean_data(df):
+
     df = df.copy()
+
+  
     df = df.drop(columns=["Unnamed: 0"])
-    df = df.drop_duplicates()
     df = df.dropna()
+
+    puntuacion = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
 
     df["sexo"] = df["sexo"].str.strip().str.lower()
 
     df["tipo_de_emprendimiento"] = df["tipo_de_emprendimiento"].str.strip().str.lower()
+    df["tipo_de_emprendimiento"] = df["tipo_de_emprendimiento"].str.translate(
+        str.maketrans(puntuacion, " " * len(puntuacion))
+    )
+    df["tipo_de_emprendimiento"] = (
+        df["tipo_de_emprendimiento"].str.replace(r"\s+", " ", regex=True).str.strip()
+    )
 
     df["idea_negocio"] = df["idea_negocio"].str.strip().str.lower()
+    df["idea_negocio"] = df["idea_negocio"].str.translate(
+        str.maketrans(puntuacion, " " * len(puntuacion))
+    )
+    df["idea_negocio"] = (
+        df["idea_negocio"].str.replace(r"\s+", " ", regex=True).str.strip()
+    )
 
     df["barrio"] = df["barrio"].str.strip().str.lower()
+    df["barrio"] = df["barrio"].str.translate(
+        str.maketrans(puntuacion, " " * len(puntuacion))
+    )
+    df["barrio"] = df["barrio"].str.replace(r"\s+", " ", regex=True).str.strip()
 
     df["línea_credito"] = df["línea_credito"].str.strip().str.lower()
     df["línea_credito"] = df["línea_credito"].str.replace(" ", "_", regex=False)
     df["línea_credito"] = df["línea_credito"].str.replace("-", "_", regex=False)
     df["línea_credito"] = df["línea_credito"].str.replace(".", "_", regex=False)
-    df["línea_credito"] = df["línea_credito"].str.replace(r"_+$", "", regex=True)
     df["línea_credito"] = df["línea_credito"].str.replace(r"_+", "_", regex=True)
 
     df["monto_del_credito"] = df["monto_del_credito"].str.replace("$", "", regex=False)
     df["monto_del_credito"] = df["monto_del_credito"].str.replace(",", "", regex=False)
-    df["monto_del_credito"] = df["monto_del_credito"].str.replace(".", "", regex=False)
     df["monto_del_credito"] = df["monto_del_credito"].str.strip()
-    df["monto_del_credito"] = df["monto_del_credito"].astype(int)
+    df["monto_del_credito"] = df["monto_del_credito"].astype(float).astype(int)
 
     df["fecha_de_beneficio"] = pd.to_datetime(
         df["fecha_de_beneficio"],
@@ -53,17 +72,11 @@ def clean_data(df):
 def save_data(df, output_file):
 
     df = df.copy()
-    df.to_csv(output_file, index=False)
+    df.to_csv(output_file, sep=";", index=False)
+
+
 def pregunta_01():
-    """
-    Realice la limpieza del archivo "files/input/solicitudes_de_credito.csv".
-    El archivo tiene problemas como registros duplicados y datos faltantes.
-    Tenga en cuenta todas las verificaciones discutidas en clase para
-    realizar la limpieza de los datos.
 
-    El archivo limpio debe escribirse en "files/output/solicitudes_de_credito.csv"
-
-    """
     archivo_entrada = "files/input/solicitudes_de_credito.csv"
     archivo_salida = "files/output/solicitudes_de_credito.csv"
 
@@ -74,4 +87,3 @@ def pregunta_01():
     save_data(df, archivo_salida)
 
     return df
-print(pregunta_01())
